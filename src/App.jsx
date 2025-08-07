2aea0b1e3871cbc567245da8b1b7898d9929810a
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-07 19:53:24 -0300

    router applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -44,7 +45,7 @@ function App() {
 
   const onAddTaskSubmit = (title, description) => {
     const newTask = {
-      id: tasks.length + 1,
+      id: v4(),
       title: title,
       description: description,
       isCompleted: false,
@@ -55,7 +56,7 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-slate-800 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciamento de Tarefas
         </h1>
