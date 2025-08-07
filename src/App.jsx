0a67995c7b8bd500fbdc32ec7b65eb76489f4ef8
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-07 15:51:54 -0300

    feat: props passed to tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Estudar react routers",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Inglês",
+      description: "Estudar inglês para fins profissionais",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Beber água",
+      description: "Manter o rim saudável",
+      isCompleted: false,
+    },
+  ]);
+
   return (
     <div className="w-screen h-screen bg-slate-800 flex justify-center p-6">
       <div className="w-[500px]">
@@ -9,7 +31,7 @@ function App() {
           Gerenciamento de Tarefas
         </h1>
         <AddTask />
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,27 @@
-function Tasks() {
+import PropTypes from "prop-types";
+
+function Tasks(props) {
+  console.log(props);
   return (
     <div>
-      <h1 className="text-2xl text-red-400">Tasks</h1>
+      <ul className="space-y-3 p-6 bg-slate-200 rounded-lg shadow">
+        {props.tasks.map((task) => (
+          <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+Tasks.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default Tasks;
