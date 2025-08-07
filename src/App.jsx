599213228316ca6a72712f96d729b61861f55329
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-07 14:26:06 -0300

    feat: tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Tasks from "./components/Tasks";
 
 function App() {
   return (
-    <>
-      <AddTask />
-      <Tasks />
-    </>
+    <div className="w-screen h-screen bg-slate-800 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciamento de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
+    </div>
   );
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,7 @@
 function Tasks() {
   return (
     <div>
-      <h1>Tasks</h1>
+      <h1 className="text-2xl text-red-400">Tasks</h1>
     </div>
   );
 }
